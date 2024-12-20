7e734393670768d15f0b737ef4c7ad63fbeac71b
arbab-killer <mr.crazymemes76@gmail.com>
2024-12-20 09:52:15 +0530

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://samplepaper.arbab.shop/', // Replace with your site's URL
+    generateRobotsTxt: true,           // Generate robots.txt file
+    exclude: ['/admin/*'],             // Exclude specific routes if needed
+  };
+  
